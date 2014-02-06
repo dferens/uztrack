4f20431d63edcc912b65fed47353ce7e5da8b530
Ferens Dmitriy <ferensdima@gmail.com>
2014-02-06 02:53:36 +0200

    Replaced ui-calendar with cal-heatmap

--- uztrack/static/js/trackedway_detail.js
+++ uztrack/static/js/trackedway_detail.js
@@ -1,38 +1,87 @@
-var myApp = angular.module('myApp', ['ui.calendar']);
+var myApp = angular.module('myApp', []);
 
 myApp.controller('WayDetailCtrl', function($scope, $http, urls) {
-    $scope.uiConfig = {
-        calendar: {
-            firstDay: 1,
-            editable: true,
-            header:{
-              left: '',
-              center: 'title',
-              right: 'today prev,next'
-            },
-        }
+    $scope.getDate = function(date_str) {
+        return new Date(Date.parse(date_str));
     };
-    $scope.events = [];
-    $scope.eventSources = [$scope.events];
-    $scope.urls = urls;
-    $scope.tracked_way_id = window.location.pathname.split('/track/')[1].slice(0, -1);
-
-    $scope.loadEvents = function() {
+    $scope.getTrackedWayId = function() {
+        var temp = window.location.pathname;
+        temp = temp.split('/track/')[1];
+        return temp.slice(0, -1);
+    }
+    $scope.addHistory = function(history) {
+        var departure_date = history.departure_date;
+        var places_count = history.last_snapshot.total_places_count;
+        $scope.data.push({
+            date: $scope.getDate(departure_date),
+            places_count: places_count,
+        });
+    };
+    $scope.init = function() {
+        $scope.data = [];
+        $scope.urls = urls;
+        $scope.tracked_way_id = $scope.getTrackedWayId();
+        
         var cfg = {params: {tracked_way: $scope.tracked_way_id}};
         $http.get(urls.histories, cfg).success(function(data){
             $.each(data, function(i, history) {
                 $scope.addHistory(history);
             });
+            $scope.initHeatmap();
         });
     };
-    $scope.addHistory = function(history) {
-        var departure_date = history.departure_date;
-        var places_count = history.last_snapshot.total_places_count;
-        $scope.events.push({
-            title: places_count.toString(),
-            start: departure_date,
+    $scope.initHeatmap = function() {
+        var getStartDate = function() {
+            return $scope.data[0].date;
+        };
+        var getLastDate = function() {
+            return $scope.data[$scope.data.length - 1].date;
+        }
+        var getRange = function() {
+            var lastMonth = getLastDate().getMonth() + 1;
+            var firstMonth = getStartDate().getMonth();
+            return lastMonth - firstMonth;
+        };
+        var calculateLegend = function() {
+            return [50, 100, 250, 500]
+        };
+        var generateCalData = function() {
+            var result = {}, seconds;
+            $.each($scope.data, function(i, value) {
+                seconds = value.date.getTime() / 1000;
+                result[seconds] = value.places_count;
+            });
+            return result;
+        };
+        $scope.heatmap = new CalHeatMap();
+        $scope.heatmap.init({
+            itemSelector: '#heatmap',
+            domain: 'month',
+            subDomain: 'x_day',
+            cellSize: 20,
+            start: getStartDate(),
+            range: getRange(),
+            highlight: 'now',
+            data: generateCalData(),
+            subDomainTextFormat: '%d',
+            considerMissingDataAsZero: false,
+            domainLabelFormat: '%b %Y',
+            verticalOrientation: true,
+            legendVerticalPosition: 'bottom',
+            legendHorizontalPosition: 'right',
+            legendMargin: [15, 3, 0, 0],
+            legendCellSize: 13,
+            legendOrientation: 'horizontal',
+            legend: calculateLegend(),
+            label: {
+                position: 'left',
+                width: 80,
+                offset: {x: 15, y: 80},
+            },
+            domainGutter: 0,
+            domainDynamicDimension: true,
         });
     };
 
-    $scope.loadEvents();
+    $scope.init();
 });
