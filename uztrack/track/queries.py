92d5ff633052a9f2d33443063944631717a0c342
Ferens Dmitriy <ferensdima@gmail.com>
2014-02-10 19:53:19 +0200

    More docs

--- uztrack/track/queries.py
+++ uztrack/track/queries.py
@@ -2,25 +2,36 @@ from datetime import date
 
 from django.conf import settings
 
-from .models import TrackedWayDayHistory, TrackedWayDayHistorySnapshot
+from .models import TrackedWayDayHistory as History, \
+                    TrackedWayDayHistorySnapshot as Snapshot
 
 
 def get_closest_histories(tracked_way):
+    """
+    Returns collection of closest :class:`History` objects for a given way.
+    Creates new records if needed.
+    """
     till_date = date.today() + settings.TICKETS_SEARCH_RANGE
-    dates = tracked_way.next_dates(till_date)
-    found_histories = TrackedWayDayHistory.objects.filter(departure_date__in=dates).all()
+    closest_dates = tracked_way.next_dates(till_date)
+    found_histories = History.objects.filter(departure_date__in=closest_dates)
     found_dates = found_histories.values_list('departure_date', flat=True)
-    not_found_dates = filter(lambda d: d not in found_dates, dates)
+    not_found_dates = filter(lambda d: d not in found_dates, closest_dates)
     histories_list = list(found_histories)
     for not_found_date in not_found_dates:
-        histories_list.append(TrackedWayDayHistory.objects
-                                                  .create(tracked_way=tracked_way,
-                                                          departure_date=not_found_date))
+        histories_list.append(
+            History.objects.create(tracked_way=tracked_way,
+                                   departure_date=not_found_date)
+        )
     return histories_list
 
 
 def create_snapshot(history, stations_routes):
-    args = dict(history=history,
-                total_places_count=stations_routes.seats_count,
-                snapshot_data=stations_routes._raw)
-    return TrackedWayDayHistorySnapshot.objects.create(**args)
+    """
+    Creates :class:`Snapshot` snapshot from a given api call result.
+
+    :type history: :class:`History`
+    :type stations_routes: :class:`core.uzgovua.data.StationsRoutes`
+    """
+    return Snapshot.objects.create(history=history,
+                                   total_places_count=stations_routes.seats_count,
+                                   snapshot_data=stations_routes._raw)
