77866784c34052a26fbe4888cb12dbc23a0ca83d
Ferens Dmitriy <ferensdima@gmail.com>
2014-02-16 19:23:43 +0200

    Edited celery app name

--- uztrack/celeryapp.py
+++ uztrack/celeryapp.py
@@ -5,10 +5,9 @@ from django.conf import settings
 from celery import Celery
 from celery.signals import celeryd_init
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uztrack.settings.production")
 
-app = Celery('proj')
+app = Celery(settings.SITE_NAME)
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
