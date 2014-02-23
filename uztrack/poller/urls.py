7e8115d55e3b05df2b4f98d34949a0b2be83861c
Ferens Dmitriy <ferensdima@gmail.com>
2014-02-23 14:56:19 +0200

    Fixed urls

--- uztrack/poller/urls.py
+++ uztrack/poller/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'stats/$', views.TotalStatsView.as_view(), name='total-stats'),
+    url(r'^stats/$', views.TotalStatsView.as_view(), name='total-stats'),
 )
--- uztrack/track/urls.py
+++ uztrack/track/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'way/$', views.WayListView.as_view(), name='way_list'),
-    url(r'way/create/$', views.WayCreateView.as_view(), name='way_create'),
-    url(r'way/(?P<pk>\d+)/$', views.WayDetailView.as_view(), name='way_detail'),
+    url(r'^way/$', views.WayListView.as_view(), name='way_list'),
+    url(r'^way/create/$', views.WayCreateView.as_view(), name='way_create'),
+    url(r'^way/(?P<pk>\d+)/$', views.WayDetailView.as_view(), name='way_detail'),
 
-    url(r'track/$', views.TrackedWayListView.as_view(), name='track_list'),
-    url(r'track/create/$', views.TrackedWayCreateView.as_view(), name='track_create'),
-    url(r'track/(?P<pk>\d+)/$', views.TrackedWayDetailView.as_view(), name='track_detail'),
-    url(r'track/(?P<pk>\d+)/edit/$', views.TrackedWayEditView.as_view(), name='track_edit'),
+    url(r'^track/$', views.TrackedWayListView.as_view(), name='track_list'),
+    url(r'^track/create/$', views.TrackedWayCreateView.as_view(), name='track_create'),
+    url(r'^track/(?P<pk>\d+)/$', views.TrackedWayDetailView.as_view(), name='track_detail'),
+    url(r'^track/(?P<pk>\d+)/edit/$', views.TrackedWayEditView.as_view(), name='track_edit'),
 )
