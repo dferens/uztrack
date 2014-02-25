9935999e34e69d8e427c87add1558e954cc91458
Ferens Dmitriy <ferensdima@gmail.com>
2014-02-25 02:03:08 +0200

    Improved stats page

--- uztrack/poller/views.py
+++ uztrack/poller/views.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 
 from track.models import TrackedWay, \
                          TrackedWayDayHistorySnapshot as Snapshot 
-from .utils import get_last_snapshot_elapsed
 
 
 class TotalStatsView(TemplateView):
@@ -16,26 +15,30 @@ class TotalStatsView(TemplateView):
     template_name = 'poller/total_stats.html'
 
     def calc_history_stats(self, history):
+        last_snapshot = history.snapshots.last()
         return {
             'history': history,
             'snapshots_count': history.snapshots.count(),
-            'relevance': get_last_snapshot_elapsed(history),
+            'relevance': last_snapshot.made_on,
         }
 
     def get_context_data(self):
         last_snapshot_datetime = Snapshot.objects.last().made_on
         tracked_ways = []
         for tracked_way in TrackedWay.objects.all():
-            histories = [self.calc_history_stats(h) for h in tracked_way.histories.all()]
-            max_relevance = min(h['relevance'] for h in histories)
-            snapshots_count = sum(h['snapshots_count'] for h in histories)
-            tracked_ways.append({'tracked_way': tracked_way,
-                                 'histories': histories,
-                                 'relevance': max_relevance,
-                                 'snapshots_count': snapshots_count})
+            histories = [self.calc_history_stats(history)
+                         for history in tracked_way.histories.all()
+                         if history.snapshots.exists()]
+            if histories:
+                max_relevance = max(h['relevance'] for h in histories)
+                snapshots_count = sum(h['snapshots_count'] for h in histories)
+                tracked_ways.append({'tracked_way': tracked_way,
+                                     'histories': histories,
+                                     'relevance': max_relevance,
+                                     'snapshots_count': snapshots_count})
         context = {
             'tracked_ways': tracked_ways,
-            'relevance': min(t['relevance'] for t in tracked_ways),
+            'relevance': max(t['relevance'] for t in tracked_ways),
             'snapshots_count': sum(t['snapshots_count'] for t in tracked_ways),
         }
         return context
